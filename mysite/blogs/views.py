c132b15c5fc468abffc64861863559fbc4cf68af
int_boy <intboy.1423@gmail.com>
2017-09-04 11:21:40 +0800

    实现对每个blog页面的单独显示

--- mysite/blogs/views.py
+++ mysite/blogs/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from django.http import Http404
 # Create your views here.
+from .models import Blog
+
+def detail(request, id):
+    try:
+        blog = Blog.objects.get(id=str(id))
+    except Blog.DoesNotExist:
+        raise Http404
+    return render(request, 'blogs/blog.html', {'blog':blog})
