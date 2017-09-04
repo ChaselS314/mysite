88445d3555e2755a7970a9b0a88a73a885bab7d3
int_boy <intboy.1423@gmail.com>
2017-09-04 10:20:17 +0800

    使主页可以显示自定义的blog

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 
 
+from blogs.models import Blog, Label
+# Create your views here.
+
 def home(request):
-    return render(request, 'index.html')
+    blogs_list = Blog.objects.all()
+    return render(request, 'index.html', {'blogs_list' : blogs_list})
