40d380c06127f35bb89bc69c551199ac6db6f482
int_boy <intboy.1423@gmail.com>
2017-09-04 22:52:57 +0800

    增加分页功能

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 
-
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger # 分页
 from blogs.models import Blog, Label
 # Create your views here.
 
 def home(request):
-    blogs_list = Blog.objects.all()
+    blogs = Blog.objects.all()
+    paginator = Paginator(blogs, 5)
+    page = request.GET.get('page')
+    try:
+        blogs_list = paginator.page(page)
+    except PageNotAnInteger :
+        blogs_list = paginator.page(1)
+    except EmptyPage:
+        blogs_list = paginator.paginator(paginator.num_pages)
+
     return render(request, 'index.html', {'blogs_list' : blogs_list})
